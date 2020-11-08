3a51966fe6368fe2dabf40190d3848610ca08548
Nick Smith <nick@panda.home>
2020-11-08 21:45:34 +0000

    deployment script

--- Raspberry_Pi/deploy/README.txt
+++ Raspberry_Pi/deploy/README.txt
@@ -1,6 +1,9 @@
 Metriful sensors
 
 Set IoT_cloud_logging.py to run as a service, by installing the sensor_logging_service.sh as a service using systemctl. Unit file is sensor_logging_service.service
+Mandatory arg is the API key
+
+sudo ./install-service.sh <API-KEY>
 
 Instructions as per https://www.linode.com/docs/quick-answers/linux/start-service-at-boot/,
 but in summary:
--- Raspberry_Pi/deploy/install-service.sh
+++ Raspberry_Pi/deploy/install-service.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 # install systemd service - run as root (sudo)
-
 SERVICE=sensor_logging_service
 
-# insert api-key from command line into service file
-sed 's/xxxxx/$1/g' template.service > $SERVICE.service
+if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
+    echo "Usage: install-service.sh <API KEY> [<PARTICLE SENSOR TYPE>] - exiting"
+    exit 1
+fi
+
+if [ "$#" -eq 1 ]; then
+    # only API key provided
+    # insert api-key from command line into service file
+    sed "s/xxxxx/$1/g" template.service > /tmp/$SERVICE.tmp
+    sed "s/yyyyy//g" /tmp/$SERVICE.tmp > $SERVICE.service
+else
+    # API key & sensor type provided
+    sed "s/xxxxx/$1/g" template.service > /tmp/$SERVICE.tmp
+    sed "s/yyyyy/Environment=\"PARTICLE_SENSOR_TYPE=$2\"/g" /tmp/$SERVICE.tmp > $SERVICE.service
+fi
+rm /tmp/$SERVICE.tmp
 
 cp $SERVICE.sh /usr/bin/
 chmod +x /usr/bin/$SERVICE.sh
 cp $SERVICE.service /etc/systemd/system/
 chmod 644 /etc/systemd/system/$SERVICE.service
 
-systemctl start $SERVICE
 systemctl enable $SERVICE
+systemctl start $SERVICE
 systemctl status $SERVICE
