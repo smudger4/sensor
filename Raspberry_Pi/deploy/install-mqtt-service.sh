88475a2c1a512af3db072c6e37d9bb7af83ccedc
Nick Smith <smudger4@gmail.com>
2020-11-28 17:03:13 +0000

    updates to support particle sensor

--- Raspberry_Pi/deploy/install-mqtt-service.sh
+++ Raspberry_Pi/deploy/install-mqtt-service.sh
@@ -2,16 +2,24 @@
 # install systemd service - run as root (sudo)
 SERVICE=mqtt_logging_service
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: install-service.sh <SENSOR NAME> <BROKER IP> - exiting"
+if [ "$#" -lt 2 ] || [ "$#" -gt 3 ]; then
+    echo "Usage: install-mqtt-service.sh <SENSOR NAME> <BROKER ADDR> [<PARTICLE SENSOR>] - exiting"
     exit 1
 fi
 
-# sensor name & sensor type provided
-sed "s/xxxxx/$1/g" template-mqtt.service > /tmp/$SERVICE.tmp
-sed "s/yyyyy/$2/g" /tmp/$SERVICE.tmp > $SERVICE.service
+if [ "$#" -eq 2 ]; then
+    # sensor name & sensor type provided
+    sed "s/xxxxx/$1/g" template-mqtt.service > /tmp/$SERVICE-1.tmp
+    sed "s/yyyyy/$2/g" /tmp/$SERVICE-1.tmp > /tmp/$SERVICE-2.tmp
+    sed "s/zzzzz//g" /tmp/$SERVICE-2.tmp > $SERVICE.service
+else
+    # sensor name, sensor type & particle sensor provided
+    sed "s/xxxxx/$1/g" template-mqtt.service > /tmp/$SERVICE-1.tmp
+    sed "s/yyyyy/$2/g" /tmp/$SERVICE-1.tmp > /tmp/$SERVICE-2.tmp
+    sed "s/zzzzz/$3/g" /tmp/$SERVICE-2.tmp > $SERVICE.service
+fi
 
-rm /tmp/$SERVICE.tmp
+rm /tmp/$SERVICE-*.tmp
 
 cp $SERVICE.sh /usr/bin/
 chmod +x /usr/bin/$SERVICE.sh
--- Raspberry_Pi/deploy/mqtt_logging_service.sh
+++ Raspberry_Pi/deploy/mqtt_logging_service.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
+# usage: mqtt_logging_service.sh <SENSOR NAME> <BROKER ADDR> [<PARTICLE SENSOR>]
+
+if [ "$#" -lt 2 ] || [ "$#" -gt 3 ]; then
+    echo "Usage: mqtt_logging_service.sh <SENSOR NAME> <BROKER ADDR> [<PARTICLE
+SENSOR>] - exiting"
+    exit 1
+fi
 
 DATE=`date '+%Y-%m-%d %H:%M:%S'`
 echo "MQTT logging service started at ${DATE}" | systemd-cat -p info
 cd /home/pi/code/github/sensor/Raspberry_Pi
-/usr/bin/python3 mqtt_client.py $1 $2 | /usr/bin/logger
+
+if [ "$#" -eq 2 ]; then
+    /usr/bin/python3 mqtt_client.py $1 $2 | /usr/bin/logger
+else
+    /usr/bin/python3 mqtt_client.py $1 $2 --particle $3 | /usr/bin/logger
+fi
